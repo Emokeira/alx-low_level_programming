f58e902e3e2c5262c938967a958c2d7fe5e67ce3
Emokeira <edithmokeira7@gmail.com>
2023-09-08 13:15:56 -0700

    Added: 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
