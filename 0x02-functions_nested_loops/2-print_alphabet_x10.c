02849c53b97cc199bb5c3005e10a69abd9961e0f
Emokeira <edithmokeira7@gmail.com>
2023-09-13 10:38:14 -0700

    Added : 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - A function that prints
+ * the alphabet, in lowercase 10x
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		int i = 97;
+
+		while (i <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
