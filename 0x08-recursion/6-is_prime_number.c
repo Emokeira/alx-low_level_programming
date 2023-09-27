1a38242c9faee068177f67bf900b5ccc74c76af5
Emokeira <edithmokeira7@gmail.com>
2023-09-27 11:50:30 -0700

    Added: A function that returns/checks for prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * prime2 - a function that returns 1
+ * @a: same number as n
+ * @b: the number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ */
+int prime2(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - a function that checks if a number is prime
+ * @n: input integer
+ *
+ * Return: On success 1.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
+}
