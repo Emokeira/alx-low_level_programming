7428a3c29769b93a96ac44aaa7d3e8e0c5b287f0
Emokeira <edithmokeira7@gmail.com>
2023-09-29 13:30:20 -0700

    Added: A program that prints anumber

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the numbers of arguments
+ * @argc: Number of command line arguments
+ * @argv: Character of arrays
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
