0defaf8db99f2b5b28800e9e080202bdc515688e
Emokeira <edithmokeira7@gmail.com>
2023-10-05 12:22:26 -0700

    Added: A function that allocates memory using calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory arear to fill
+ * @b: character to copy
+ * @n: number of times to copy char b
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array using calloc
+ * @nmemb: number of elements in the array
+ * @size: size of each array
+ * Return: pointer to the newly allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *output;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	output = malloc(size * nmemb);
+	if (output == NULL)
+		return (NULL);
+	_memset(output, 0, nmemb * size);
+
+	return (output);
+}
